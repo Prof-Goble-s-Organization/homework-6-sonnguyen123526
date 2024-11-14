d32ac5a3d756b76d8de86acb93bfeeb334c6777d
sonnguyen123526 <nguyenso@dickinson.edu>
2024-11-14 09:40:16 -0500

    Commit question 1

--- src/COMP232SortableLinkedList.java
+++ src/COMP232SortableLinkedList.java
@@ -16,6 +16,30 @@ public class COMP232SortableLinkedList<E extends Comparable<E>> extends COMP232D
 	 */
 	public void insertionSortList() {
 		// Intentionally not implemented - see homework assignment.
-		throw new UnsupportedOperationException("Not yet implemented");
+		// throw new UnsupportedOperationException("Not yet implemented");
+		if (head.next == tail || head.next.next == tail) {
+			return;
+		}
+		DLLNode<E> sortOver = head.next;
+		while(sortOver.next != tail){
+			DLLNode<E> insertNode = sortOver.next;
+			if(insertNode.element.compareTo(sortOver.element)> 0){
+				sortOver = sortOver.next;
+			}
+			else{
+				sortOver.next = insertNode.next;
+				insertNode.next.prev = sortOver;
+				DLLNode<E> position = head.next;
+				while(position != sortOver.next && insertNode.element.compareTo(position.element)>0){
+					position = position.next;
+				}
+				insertNode.next = position;
+				insertNode.prev = position.prev;
+				position.prev.next = insertNode;
+				position.prev = insertNode;
+			}
+		}
+
+
 	}
 }
